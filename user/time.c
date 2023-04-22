65ec2b758955a764c2d4898b67e3aef4f3907c04
Bernardgc25 <Bernard_gcruz@yahoo.com>
2023-04-22 15:21:53 -0700

    Add files via upload

---
+++ user/time.c
@@ -0,0 +1,44 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+   int main(int argc, char *argv[])
+  {
+    //empty command line
+    if(argc == 0)
+      {
+          exit(0);
+      }
+
+    //process command
+    else
+      {
+          //printf("ARGV: %s\n", argv[0]);
+          int pid = fork();
+
+          if(pid > 0)
+            {
+              //printf("parent: child=%d\n", pid);
+              pid = wait((int *)0);
+              //printf("child %d is done\n",pid);
+            }
+          else if(pid == 0)
+            {
+              //printf("child: exiting\n");
+              //printf("real time in ticks: %d\n", uptime());
+              //exit(0);
+              exec("ls", &argv[1]);
+              //printf("exec: %d\n", exec("ls", &argv[1]));
+            }
+          else
+            {
+              printf("fork error \n");
+            }
+
+
+      }
+        //display uptime
+        printf("Real-time in ticks: %d\n", uptime());
+        exit(0);
+   }
